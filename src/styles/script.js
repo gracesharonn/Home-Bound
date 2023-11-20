0cc5eb61082ec287a880fe3a5df600171d542be3
Zanderskier <alexnfails@gmail.com>
2023-11-20 13:38:56 -0800

    format javascript

--- src/styles/script.js
+++ src/styles/script.js
@@ -1,120 +1,117 @@
 /* Navbar effects */
-var prevScrollpos = window.pageYOffset;
-        window.onscroll = function() {
-            var currentScrollPos = window.pageYOffset;
-            if (currentScrollPos === 0) {
-                document.querySelector(".NavigationBar").style.top = "0";
-            } else if (prevScrollpos > currentScrollPos) {
-                document.querySelector(".NavigationBar").style.top = "-100px";
-            } else {
-                document.querySelector(".NavigationBar").style.top = "-100px"; // Adjust this value based on your header height
-            }
-            prevScrollpos = currentScrollPos;
-        }
+var prevScrollpos = window.pageYOffset
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset
+  if (currentScrollPos === 0) {
+    document.querySelector('.NavigationBar').style.top = '0'
+  } else if (prevScrollpos > currentScrollPos) {
+    document.querySelector('.NavigationBar').style.top = '-100px'
+  } else {
+    document.querySelector('.NavigationBar').style.top = '-100px' // Adjust this value based on your header height
+  }
+  prevScrollpos = currentScrollPos
+}
 /* Services Menu Bar */
-document.addEventListener('DOMContentLoaded', function() {
-    const servicesButton = document.getElementById('ServicesButton');
-    const serviceMenu = document.querySelector('.ServiceMenu');
-    const closeButton = document.getElementById('x-button');
+document.addEventListener('DOMContentLoaded', function () {
+  const servicesButton = document.getElementById('ServicesButton')
+  const serviceMenu = document.querySelector('.ServiceMenu')
+  const closeButton = document.getElementById('x-button')
 
-    servicesButton.addEventListener('click', function() {
-        serviceMenu.classList.add('active');
-        event.preventDefault();
-    });
+  servicesButton.addEventListener('click', function () {
+    serviceMenu.classList.add('active')
+    event.preventDefault()
+  })
 
-    closeButton.addEventListener('click', function() {
-        serviceMenu.classList.remove('active');
-    });
-});
+  closeButton.addEventListener('click', function () {
+    serviceMenu.classList.remove('active')
+  })
+})
 
 // Zipcode validation
-function validateZipCode() {
-    var zipCodeInput = document.getElementById("zip_code");
-    var zipCodeValue = zipCodeInput.value;
+function validateZipCode () {
+  var zipCodeInput = document.getElementById('zip_code')
+  var zipCodeValue = zipCodeInput.value
 
-// Regular expression for a 5-digit ZIP code (US format)
-var zipCodePattern = /^\d{5}$/;
+  // Regular expression for a 5-digit ZIP code (US format)
+  var zipCodePattern = /^\d{5}$/
 
-if (!zipCodePattern.test(zipCodeValue)) {
-    alert("Please enter a valid 5-digit ZIP code.");
-    zipCodeInput.focus();
-    return false;
-}
+  if (!zipCodePattern.test(zipCodeValue)) {
+    alert('Please enter a valid 5-digit ZIP code.')
+    zipCodeInput.focus()
+    return false
+  }
 
-return true;
+  return true
 }
 // Shelter service question functions
-function showAdditionalQuestions() {
-    var serviceSelect = document.getElementById("service");
-    var shelterQuestions = document.getElementById("shelterQuestions");
-    var daycareQuestions = document.getElementById("daycareQuestions");
+function showAdditionalQuestions () {
+  var serviceSelect = document.getElementById('service')
+  var shelterQuestions = document.getElementById('shelterQuestions')
+  var daycareQuestions = document.getElementById('daycareQuestions')
 
-if (serviceSelect.value === "Shelter") {
-    shelterQuestions.style.display = "block";
-    daycareQuestions.style.display = "none";
-} else if (serviceSelect.value === "Daycare") {
-    daycareQuestions.style.display = "block";
-    shelterQuestions.style.display = "none";
-} else {
-    shelterQuestions.style.display = "none";
-    daycareQuestions.style.display = "none";
+  if (serviceSelect.value === 'Shelter') {
+    shelterQuestions.style.display = 'block'
+    daycareQuestions.style.display = 'none'
+  } else if (serviceSelect.value === 'Daycare') {
+    daycareQuestions.style.display = 'block'
+    shelterQuestions.style.display = 'none'
+  } else {
+    shelterQuestions.style.display = 'none'
+    daycareQuestions.style.display = 'none'
+  }
 }
+
+//Shelter JSON Displayed
+function fetchAndDisplayShelters () {
+  // Assuming the JSON file is one level up from the current directory
+  const jsonFilePath = '../../JSON/services.json'
+
+  fetch(jsonFilePath)
+    .then(response => response.json())
+    .then(data => {
+      // Handle the JSON data and display businesses
+      displayShelters(data.shelters)
+    })
+    .catch(error => console.error('Error fetching JSON:', error))
 }
 
-  //Shelter JSON Displayed
-  function fetchAndDisplayShelters() {
-    // Assuming the JSON file is one level up from the current directory
-    const jsonFilePath = '../../JSON/services.json';
-  
-    fetch(jsonFilePath)
-        .then(response => response.json())
-        .then(data => {
-            // Handle the JSON data and display businesses
-            displayShelters(data.shelters);
-            
-        })
-        .catch(error => console.error('Error fetching JSON:', error));
-  }
+//Medical JSON Displayed
+function fetchAndDisplayMedical () {
+  // Assuming the JSON file is one level up from the current directory
+  const jsonFilePath = '../../JSON/services.json'
 
-  //Medical JSON Displayed
-  function fetchAndDisplayMedical() {
-    // Assuming the JSON file is one level up from the current directory
-    const jsonFilePath = '../../JSON/services.json';
-  
-    fetch(jsonFilePath)
-        .then(response => response.json())
-        .then(data => {
-            // Handle the JSON data and display businesses
-            displayShelters(data.medical);
-            
-        })
-        .catch(error => console.error('Error fetching JSON:', error));
-  }
+  fetch(jsonFilePath)
+    .then(response => response.json())
+    .then(data => {
+      // Handle the JSON data and display businesses
+      displayShelters(data.medical)
+    })
+    .catch(error => console.error('Error fetching JSON:', error))
+}
 
-  //Food JSON Displayed
-  function fetchAndDisplayFood() {
-    // Assuming the JSON file is one level up from the current directory
-    const jsonFilePath = '../../JSON/services.json';
-  
-    fetch(jsonFilePath)
-        .then(response => response.json())
-        .then(data => {
-            // Handle the JSON data and display businesses
-            displayShelters(data.food);
-            
-        })
-        .catch(error => console.error('Error fetching JSON:', error));
-  }
-  
-  function displayShelters(shelters) {
-    const sheltersContainer = document.getElementById('sheltersContainer');
-    sheltersContainer.innerHTML = ''; // Clear previous content
+//Food JSON Displayed
+function fetchAndDisplayFood () {
+  // Assuming the JSON file is one level up from the current directory
+  const jsonFilePath = '../../JSON/services.json'
 
-    shelters.forEach((shelter, index) => {
-        const shelterDiv = document.createElement('div');
-        shelterDiv.classList.add('sheltersContainer');
+  fetch(jsonFilePath)
+    .then(response => response.json())
+    .then(data => {
+      // Handle the JSON data and display businesses
+      displayShelters(data.food)
+    })
+    .catch(error => console.error('Error fetching JSON:', error))
+}
 
-        let content = `
+function displayShelters (shelters) {
+  const sheltersContainer = document.getElementById('sheltersContainer')
+  sheltersContainer.innerHTML = '' // Clear previous content
+
+  shelters.forEach((shelter, index) => {
+    const shelterDiv = document.createElement('div')
+    shelterDiv.classList.add('sheltersContainer')
+
+    let content = `
             <br>
             <h4>${shelter.businessName}</h4>
             <p>${shelter.location.street} ${shelter.location.city} ${shelter.location.state} ${shelter.location.zipcode}</p>
@@ -125,149 +122,123 @@ if (serviceSelect.value === "Shelter") {
             <p class="descriptionParagraph">"${shelter.description}"</p>
             <br>
             <hr>
-        `;
+        `
 
-        // Check if email exists before adding it
-        if (!shelter.contacts.email) {
-            content = content.replace('<p class="emailParagraph">', '').replace('</p>', '');
-        }
+    // Check if email exists before adding it
+    if (!shelter.contacts.email) {
+      content = content
+        .replace('<p class="emailParagraph">', '')
+        .replace('</p>', '')
+    }
 
-        // Check if website exists before adding it
-        if (!shelter.website) {
-            content = content.replace('<p class="websiteParagraph">', '').replace('</p>', '');
-        }
+    // Check if website exists before adding it
+    if (!shelter.website) {
+      content = content
+        .replace('<p class="websiteParagraph">', '')
+        .replace('</p>', '')
+    }
 
-        shelterDiv.innerHTML = content;
-        sheltersContainer.appendChild(shelterDiv);
-    });
+    shelterDiv.innerHTML = content
+    sheltersContainer.appendChild(shelterDiv)
+  })
 }
 
 //Open new tab for website
-function openInNewTab(url) {
-    event.preventDefault();
-    window.open(url, '_blank');
+function openInNewTab (url) {
+  event.preventDefault()
+  window.open(url, '_blank')
 }
 
 //onSubmit of form
 
-function submitForm() {
+function submitForm () {
+  var service = document.getElementById('service').value
 
-    var service = document.getElementById("service").value;
+  //check if service
+  if (service === 'Food') {
+  } else if (service === 'Shelter') {
+    openShelterSearch(service)
+  } else if (service === 'Medical') {
+  } else if (service === 'Daycare') {
+  } else if (service === 'Bikehub') {
+  } else if (service === 'Employment') {
+  } else {
+  }
+  //gets value from a radio button veteran
+  //var isVet = document.querySelector('input[name="veteran"]:checked').value;
 
-    //check if service
-    if(service === "Food"){
+  //window.location.href = `./services/Search.html?service=` + encodeURIComponent(isVet);
 
-    }else if(service === "Shelter"){
-        
-        openShelterSearch(service);
+  return true
+}
 
-    }else if(service === "Medical"){
+function openShelterSearch (service) {
+  var sex = document.querySelector('input[name="sex"]:checked').value
+  var isVet = document.querySelector('input[name="veteran"]:checked').value
 
+  window.location.href =
+    `./services/Search.html?service=` +
+    encodeURIComponent(service) +
+    `&sex=` +
+    encodeURIComponent(sex) +
+    `&veteran=` +
+    encodeURIComponent(isVet)
+}
 
-    }else if(service === "Daycare"){
+function searchResults () {
+  // Assuming the JSON file is one level up from the current directory
+  const jsonFilePath = '../../JSON/services.json'
 
+  const urlParse = new URLSearchParams(window.location.search)
 
-    }else if(service === "Bikehub"){
+  var service = urlParse.get('service')
 
-    }else if(service === "Employment"){
+  //check if service
+  if (service === 'Food') {
+  } else if (service === 'Shelter') {
+    var vet = urlParse.get('veteran')
+    var sex = urlParse.get('sex')
+    if (vet === 'Yes') {
+      fetch(jsonFilePath)
+        .then(response => response.json())
+        .then(data => {
+          // Handle the JSON data and display businesses
 
-    
-    }else {
-
+          displayVeteranFirst(data.shelters)
+          return
+        })
+        .catch(error => console.error('Error fetching JSON:', error))
+      fetch(jsonFilePath)
     }
-    //gets value from a radio button veteran
-    //var isVet = document.querySelector('input[name="veteran"]:checked').value;
-
-
-    //window.location.href = `./services/Search.html?service=` + encodeURIComponent(isVet);
-
-
-    return true;
+  } else if (service === 'Medical') {
+  } else if (service === 'Daycare') {
+  } else if (service === 'Bikehub') {
+  } else if (service === 'Employment') {
+  } else {
+  }
 }
 
-function openShelterSearch(service){
-    
-    var sex = document.querySelector('input[name="sex"]:checked').value;
-    var isVet = document.querySelector('input[name="veteran"]:checked').value;
+function displayVeteranFirst (businesses) {
+  // Filter veteran and non-veteran shelters
+  var veteranShelters = businesses.filter(
+    business => business.attributes.veteran === 'Yes'
+  )
+  var nonVeteranShelters = businesses.filter(
+    business => business.attributes.veteran !== 'Yes'
+  )
 
-    window.location.href = `./services/Search.html?service=` + encodeURIComponent(service) + `&sex=` + encodeURIComponent(sex) +`&veteran=` + encodeURIComponent(isVet);
-}
+  // Combine veteran and non-veteran shelters
+  var allShelters = veteranShelters.concat(nonVeteranShelters)
 
-function searchResults(){
+  // Get the sheltersContainer div
+  var vetContainer = document.getElementById('sheltersContainer')
 
-    // Assuming the JSON file is one level up from the current directory
-    const jsonFilePath = '../../JSON/services.json';
+  // Clear previous content in sheltersContainer
+  vetContainer.innerHTML = ''
 
-    const urlParse = new URLSearchParams(window.location.search);
-    
-    var service = urlParse.get("service");
-        
-        //check if service
-        if(service === "Food"){
-
-        }else if(service === "Shelter"){
-            var vet = urlParse.get("veteran");
-            var sex = urlParse.get("sex");
-            if(vet === "Yes"){
-                fetch(jsonFilePath)
-                .then(response => response.json())
-                .then(data => {
-                    // Handle the JSON data and display businesses
-                    
-                    displayVeteranFirst(data.shelters);
-                    return;
-                    
-                })
-                .catch(error => console.error('Error fetching JSON:', error));
-                fetch(jsonFilePath)
-            }else if(sex === "Female"){
-                fetch(jsonFilePath)
-                .then(response => response.json())
-                .then(data => {
-                // Handle the JSON data and display businesses
-    
-                displaySexFirst(data.shelters);
-                return;
-            
-                })
-                .catch(error => console.error('Error fetching JSON:', error));
-            }
-
-    
-        }else if(service === "Medical"){
-    
-    
-        }else if(service === "Daycare"){
-    
-    
-        }else if(service === "Bikehub"){
-    
-        }else if(service === "Employment"){
-    
-        
-        }else {
-    
-        }
-}
-
-function displayVeteranFirst(businesses){
-
-    // Filter veteran and non-veteran shelters
-    var veteranShelters = businesses.filter(business => business.attributes.veteran === "Yes");
-    var nonVeteranShelters = businesses.filter(business => business.attributes.veteran !== "Yes");
-
-    // Combine veteran and non-veteran shelters
-    var allShelters = veteranShelters.concat(nonVeteranShelters);
-
-    // Get the sheltersContainer div
-    var vetContainer = document.getElementById("sheltersContainer");
-
-    // Clear previous content in sheltersContainer
-    vetContainer.innerHTML = "";
-
-    // Display shelter information in sheltersContainer
-    allShelters.forEach(shelter => {
-        vetContainer.innerHTML += `
+  // Display shelter information in sheltersContainer
+  allShelters.forEach(shelter => {
+    vetContainer.innerHTML += `
             <div class="sheltersContainer">
             <br>
             <h4>${shelter.businessName}</h4>
@@ -279,46 +250,6 @@ function displayVeteranFirst(businesses){
             <p class="descriptionParagraph">"${shelter.description}"</p>
             <br>
             <hr>
-        `;
-    });
-}
-//Still working on
-function displaySexFirst(businesses){
-
-    // Filter veteran and non-veteran shelters
-    const urlParse = new URLSearchParams(window.location.search);
-    var sex = urlParse.get("sex");
-    var shelters;
-    var nonShelters;
-
-    //var data = urlParse.get("sex");
-    var output = document.getElementById("sheltersContainer");
-    output.innerHTML = sex;
-    
-/*
-    // Combine veteran and non-veteran shelters
-    var allShelters = shelters.concat(nonShelters);
-
-    // Get the sheltersContainer div
-    var container = document.getElementById("sheltersContainer");
-
-    // Clear previous content in sheltersContainer
-    container.innerHTML = "";
-
-    // Display shelter information in sheltersContainer
-    allShelters.forEach(shelter => {
-        container.innerHTML += `
-            <div class="sheltersContainer">
-            <br>
-            <h4>${shelter.businessName}</h4>
-            <p>${shelter.location.street} ${shelter.location.city} ${shelter.location.state} ${shelter.location.zipcode}</p>
-            <p>${shelter.contacts.phone}</p>
-            <a href="mailto:${shelter.contacts.email}" class="emailParagraph" target="_black">${shelter.contacts.email}</a>
-            <br>
-            <a href="${shelter.website}" class="websiteParagraph" onclick="openInNewTab('${shelter.website}')">${shelter.website}</a>
-            <p class="descriptionParagraph">"${shelter.description}"</p>
-            <br>
-            <hr>
-        `;
-    });*/
+        `
+  })
 }
