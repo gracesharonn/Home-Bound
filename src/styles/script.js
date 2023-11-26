3985c894cb25ad379346d0495d66306b5ec2a4a3
Grace Sharonn <gracesharonniewijaya@gmail.com>
2023-11-26 12:12:28 -0800

    moved registration html and js to main files

--- src/styles/script.js
+++ src/styles/script.js
@@ -43,6 +43,7 @@ if (!zipCodePattern.test(zipCodeValue)) {
 
 return true;
 }
+
 // Shelter service question functions
 function showAdditionalQuestions() {
     var serviceSelect = document.getElementById("service");
@@ -105,6 +106,20 @@ if (serviceSelect.value === "Shelter") {
         })
         .catch(error => console.error('Error fetching JSON:', error));
   }
+
+  //Daycare JSON Displayed
+function fetchAndDisplayDaycare () {
+  // Assuming the JSON file is one level up from the current directory
+  const jsonFilePath = '../../JSON/services.json'
+
+  fetch(jsonFilePath)
+    .then(response => response.json())
+    .then(data => {
+      // Handle the JSON data and display businesses
+      displayShelters(data.daycare)
+    })
+    .catch(error => console.error('Error fetching JSON:', error))
+}
   
   //Services Information Displayed
   function displayShelters(shelters) {
@@ -228,170 +243,6 @@ function checkPassword(){
                 enterData();
             })
     }
-/* Navbar effects */
-var prevScrollpos = window.pageYOffset
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset
-  if (currentScrollPos === 0) {
-    document.querySelector('.NavigationBar').style.top = '0'
-  } else if (prevScrollpos > currentScrollPos) {
-    document.querySelector('.NavigationBar').style.top = '-100px'
-  } else {
-    document.querySelector('.NavigationBar').style.top = '-100px' // Adjust this value based on your header height
-  }
-  prevScrollpos = currentScrollPos
-}
-/* Services Menu Bar */
-document.addEventListener('DOMContentLoaded', function () {
-  const servicesButton = document.getElementById('ServicesButton')
-  const serviceMenu = document.querySelector('.ServiceMenu')
-  const closeButton = document.getElementById('x-button')
-
-  servicesButton.addEventListener('click', function () {
-    serviceMenu.classList.add('active')
-    event.preventDefault()
-  })
-
-  closeButton.addEventListener('click', function () {
-    serviceMenu.classList.remove('active')
-  })
-})
-
-// Zipcode validation
-function validateZipCode () {
-  var zipCodeInput = document.getElementById('zip_code')
-  var zipCodeValue = zipCodeInput.value
-
-  // Regular expression for a 5-digit ZIP code (US format)
-  var zipCodePattern = /^\d{5}$/
-
-  if (!zipCodePattern.test(zipCodeValue)) {
-    alert('Please enter a valid 5-digit ZIP code.')
-    zipCodeInput.focus()
-    return false
-  }
-
-  return true
-}
-// Shelter service question functions
-function showAdditionalQuestions () {
-  var serviceSelect = document.getElementById('service')
-  var shelterQuestions = document.getElementById('shelterQuestions')
-  var daycareQuestions = document.getElementById('daycareQuestions')
-
-  if (serviceSelect.value === 'Shelter') {
-    shelterQuestions.style.display = 'block'
-    daycareQuestions.style.display = 'none'
-  } else if (serviceSelect.value === 'Daycare') {
-    daycareQuestions.style.display = 'block'
-    shelterQuestions.style.display = 'none'
-  } else {
-    shelterQuestions.style.display = 'none'
-    daycareQuestions.style.display = 'none'
-  }
-}
-
-//Shelter JSON Displayed
-function fetchAndDisplayShelters () {
-  // Assuming the JSON file is one level up from the current directory
-  const jsonFilePath = '../../JSON/services.json'
-
-  fetch(jsonFilePath)
-    .then(response => response.json())
-    .then(data => {
-      // Handle the JSON data and display businesses
-      displayShelters(data.shelters)
-    })
-    .catch(error => console.error('Error fetching JSON:', error))
-}
-
-//Medical JSON Displayed
-function fetchAndDisplayMedical () {
-  // Assuming the JSON file is one level up from the current directory
-  const jsonFilePath = '../../JSON/services.json'
-
-  fetch(jsonFilePath)
-    .then(response => response.json())
-    .then(data => {
-      // Handle the JSON data and display businesses
-      displayShelters(data.medical)
-    })
-    .catch(error => console.error('Error fetching JSON:', error))
-}
-
-//Food JSON Displayed
-function fetchAndDisplayFood () {
-  // Assuming the JSON file is one level up from the current directory
-  const jsonFilePath = '../../JSON/services.json'
-
-  fetch(jsonFilePath)
-    .then(response => response.json())
-    .then(data => {
-      // Handle the JSON data and display businesses
-      displayShelters(data.food)
-    })
-    .catch(error => console.error('Error fetching JSON:', error))
-}
-
-//Daycare JSON Displayed
-function fetchAndDisplayDaycare () {
-    // Assuming the JSON file is one level up from the current directory
-    const jsonFilePath = '../../JSON/services.json'
-  
-    fetch(jsonFilePath)
-      .then(response => response.json())
-      .then(data => {
-        // Handle the JSON data and display businesses
-        displayShelters(data.daycare)
-      })
-      .catch(error => console.error('Error fetching JSON:', error))
-  }
-
-function displayShelters (shelters) {
-  const sheltersContainer = document.getElementById('sheltersContainer')
-  sheltersContainer.innerHTML = '' // Clear previous content
-
-  shelters.forEach((shelter, index) => {
-    const shelterDiv = document.createElement('div')
-    shelterDiv.classList.add('sheltersContainer')
-
-    let content = `
-            <br>
-            <h4>${shelter.businessName}</h4>
-            <p>${shelter.location.street} ${shelter.location.city} ${shelter.location.state} ${shelter.location.zipcode}</p>
-            <p>${shelter.contacts.phone}</p>
-            <a href="mailto:${shelter.contacts.email}" class="emailParagraph" target="_black">${shelter.contacts.email}</a>
-            <br>
-            <a href="${shelter.website}" class="websiteParagraph" onclick="openInNewTab('${shelter.website}')">${shelter.website}</a>
-            <p class="descriptionParagraph">"${shelter.description}"</p>
-            <br>
-            <hr>
-        `
-
-    // Check if email exists before adding it
-    if (!shelter.contacts.email) {
-      content = content
-        .replace('<p class="emailParagraph">', '')
-        .replace('</p>', '')
-    }
-
-    // Check if website exists before adding it
-    if (!shelter.website) {
-      content = content
-        .replace('<p class="websiteParagraph">', '')
-        .replace('</p>', '')
-    }
-
-    shelterDiv.innerHTML = content
-    sheltersContainer.appendChild(shelterDiv)
-  })
-}
-
-//Open new tab for website
-function openInNewTab (url) {
-  event.preventDefault()
-  window.open(url, '_blank')
-}
 
 //onSubmit of form
 
