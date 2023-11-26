e7bf5eeef48072dc2a381e852ae68f69fd150d22
Grace Sharonn <gracesharonniewijaya@gmail.com>
2023-11-26 12:13:10 -0800

    moved registration html and js to main branch

--- src/styles/script.js
+++ src/styles/script.js
@@ -347,3 +347,60 @@ function displayVeteranFirst (businesses) {
         `
   })
 }
+
+/* Registration/ SignUp */
+const fs = require('fs');
+const json = require('../JSON/login.json');
+console.log('hello')
+console.log(
+    json,
+    fs.readFileSync('../JSON/login.json').toString()
+)
+writeJSONfile()
+function loadJSON(filename = ''){
+    return JSON.parse(
+        fs.existsSync(filename)
+            ?fs.readFileSync(filename).toString()
+            :'null'
+    )
+}
+function saveJSON(filename = '', json = '"'){
+    return fs.writeFileSync(filename,JSON.stringify(json, null, 2))
+}
+function writeJSONfile(){
+    const input = loadJSON('../JSON/login.json')
+    fetch('../pages/SignUp.html')
+    var login = {'email_address' : "edricksok101@gmail.com",
+    'password' : "password",
+    'first_name': "Edrick",
+    'last_name' : "Sok",
+    'age' : "20",
+    'sex' : "Male"
+    }
+    
+    ;[
+        {login}
+        
+].forEach(string => input.Volunteers.push(string))
+    
+    //input.Users.push(user)
+    saveJSON('../JSON/login.json',input)
+}
+/*function writeJsonfile(){
+    console.log("Running.");
+    var jsfile = require('../JSON/login.json');
+    var user = { "emailAddress" : document.getElementById("contact_info").value,
+    "password" : document.getElementById("password").value,
+    "first_name" : document.getElementById("first_name").value,
+    "last_name" : document.getElementById("last_name").value,
+    "age" : document.getElementById("age").value,
+    "sex" : document.getElementById("sex").value
+    }
+    var userstring = JSON.stringify(user);
+   
+    jsfile.writeFile("login.json",userstring, function(err, result) {
+        if(err) console.log('error',err);
+    });
+}
+*/
+
